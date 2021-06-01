f43699ef7b64321857e8d38adeaeff3f9940ebb8
= <owenpatrickdrysdale@gmail.com>
2021-06-01 20:56:32 +0000

    Add scripts, data, assets.

---
+++ installer.lua
@@ -0,0 +1,200 @@
+local reactorPeripheralType = "Bigger-Reactor"
+term.setBackgroundColour(colours.blue)
+term.setTextColour(colours.white)
+term.clear()
+
+term.setCursorPos(1,1)
+term.write("Checking & downloading libraries...")
+
+-- Check for: sUtils, button, progressBar (.lua)
+local sUtils,button
+if not fs.exists("sUtils.lua") then
+  -- sUtils doesn't exist, download it.
+  local lib,err = http.get("https://raw.githubusercontent.com/SkyTheCodeMaster/SkyDocs/ddec75606d183c743c9a92bd08d28b60f8caae3a/src/main/misc/sUtils.lua") -- Collect the sUtils package.
+  if not lib then error("sUtils: " .. err) end -- Check if we got a handle, and error if we don't.
+  -- We got the library, now write it to file.
+  local f = fs.open("sUtils.lua","w") -- Open `sUtils.lua` for writing.
+  local content = lib.readAll() lib.close() -- Read and close the web handle.
+  f.write(content) -- Write it to the file.
+  -- While we're at it, load it into our local sUtils variable.
+  sUtils = load(content,"sUtils","t",_ENV)()
+else -- Else, just `require` the file.
+  sUtils = require("sUtils")
+end
+
+if not fs.exists("button.lua") then
+  -- button doesn't exist, download it.
+  local content,err = sUtils.hread("https://raw.githubusercontent.com/SkyTheCodeMaster/SkyDocs/ddec75606d183c743c9a92bd08d28b60f8caae3a/src/main/misc/button.lua") -- Collect the button package.
+  if not content then error("button: " .. err) end -- Check if we got a handle, and error if we don't.
+  -- We got the library, now write it to file.
+  sUtils.fwrite("button.lua",content)
+  -- While we're at it, load it into our local button variable.
+  button = load(content,"sUtils","t",_ENV)()
+else -- Else, just `require` the file.
+  button = require("button")
+end
+
+if not fs.exists("progressBar.lua") then
+  -- progressBar doesn't exist, download it.
+  local content,err = sUtils.hread("https://raw.githubusercontent.com/SkyTheCodeMaster/SkyDocs/ddec75606d183c743c9a92bd08d28b60f8caae3a/src/main/misc/progressbar.lua") -- Collect the progress bar package.
+  if not content then error("pg: " .. err) end -- Check if we got a handle, and error if we don't.
+  -- We got the library, now write it to file.
+  sUtils.fwrite("progressBar.lua",content)
+  -- We don't need the progress bar api for this, so we're not gonna load it.
+end
+
+term.setCursorPos(1,2)
+term.write("Done. Downloading assets.")
+
+-- We don't need to save the installer skimg file, so we can just save it in a local variable.
+local content,err = sUtils.hread("https://github.com/SkyTheCodeMaster/cc-reactor-control/blob/main/assets/installer.skimg")
+if not content then error("installer.skimg: " .. err) end
+local installerSkimg = textutils.unserialize(content)
+
+-- Download the monitor and term skimgs
+content,err = sUtils.hread("https://github.com/SkyTheCodeMaster/cc-reactor-control/blob/main/assets/monitor.skimg")
+if not content then error("monitor.skimg: " .. err) end
+sUtils.fwrite("assets/monitor.skimg",content)
+
+content,err = sUtils.hread("https://github.com/SkyTheCodeMaster/cc-reactor-control/blob/main/assets/terminal.skimg")
+if not content then error("terminal.skimg: " .. err) end
+sUtils.fwrite("assets/terminal.skimg",content)
+
+term.setCursorPos(1,3)
+term.write("Done. Downloading scripts.")
+
+content,err = sUtils.hread("https://github.com/SkyTheCodeMaster/cc-reactor-control/blob/main/main.lua")
+if not content then error("reactor.lua: " .. err) end
+sUtils.fwrite("reactor.lua",content)
+
+local function main()
+  term.clear()
+  sUtils.asset.drawSkimg(installerSkimg)
+
+  local peripherals = peripheral.getNames()
+  local reactors,monitors,config = {},{},{}
+  for i,#peripherals do
+    local pType = peripheral.getType(peripherals[i])
+    if pType == reactorPeripheralType then
+      table.insert(reactors,peripherals[i])
+    elseif pType == "monitor" then
+      table.insert(monitors,peripherals[i])
+    end
+  end
+
+  if #reactors == 0 or #monitors == 0 then
+    for i=7,13 do
+      term.setCursorPos(13,i) 
+      term.blit("                       ","fffffffffffffffffffffff","88888888888888888888888")
+    end
+    term.setCursorPos(13,7)
+    term.blit("No reactors and/or","eeeeeeeeeeeeeeeeee","888888888888888888")
+    term.setCursorPos(13,8)
+    term.blit("monitors found.","eeeeeeeeeeeeeee","888888888888888")
+    term.setCursorPos(13,10)
+    term.blit("Attach some, and press","0000000000000000000000","8888888888888888888888")
+    term.setCursorPos(13,11)
+    term.blit("any key to continue.","00000000000000000000","88888888888888888888")
+    os.pullEvent("key")
+    return main()
+  end
+  local rSelected = 1
+  local mSelected = 1
+  -- Load the first of each type
+  term.setTextColour(colours.white)
+  term.setBackgroundColour(colours.lightGrey)
+  term.setCursorPos(13,8)
+  term.write(sUtils.cut(reactors[1],21))
+  term.setCursorPos(13,10)
+  term.write(sUtils.cut(monitors[1],21))
+  -- Reasonably, this part of the code only gets ran once, so we can initalize the buttons here.
+  -- I'm not planning to do anything w/ the button, so I can just discard the IDs.
+  button.newButton(34,8,1,1,function()
+    rSelected = rSelected - 1
+    if rSelected <= 0 then
+      rSelected = #reactors
+    elseif rSelected > #reactors then
+      rSelected = 1
+    end
+    term.setTextColour(colours.white)
+    term.setBackgroundColour(colours.lightGrey)
+    term.setCursorPos(13,8)
+    term.write(sUtils.cut(reactors[1],21))
+  end)
+  button.newButton(35,8,1,1,function()
+    rSelected = rSelected + 1
+    if rSelected <= 0 then
+      rSelected = #reactors
+    elseif rSelected > #reactors then
+      rSelected = 1
+    end
+    term.setTextColour(colours.white)
+    term.setBackgroundColour(colours.lightGrey)
+    term.setCursorPos(13,8)
+    term.write(sUtils.cut(reactors[1],21))
+  end)
+  button.newButton(34,10,1,1,function()
+    mSelected = mSelected - 1
+    if rSelected <= 0 then
+      mSelected = #monitors
+    elseif mSelected > #monitors then
+      mSelected = 1
+    end
+    term.setTextColour(colours.white)
+    term.setBackgroundColour(colours.lightGrey)
+    term.setCursorPos(13,10)
+    term.write(sUtils.cut(monitors[1],21))
+  end)
+  button.newButton(35,10,1,1,function()
+    mSelected = mSelected + 1
+    if rSelected <= 0 then
+      mSelected = #monitors
+    elseif mSelected > #monitors then
+      mSelected = 1
+    end
+    term.setTextColour(colours.white)
+    term.setBackgroundColour(colours.lightGrey)
+    term.setCursorPos(13,10)
+    term.write(sUtils.cut(monitors[1],21))
+  end)
+  -- Accept config button
+  button.newButton(17,12,3,2,function()
+    config["reactor"] = reactors[rSelected]
+    config["monitor"] = monitors[mSelected]
+    sUtils.encfwrite(".reactor.conf",config)
+    for i=7,13 do
+      term.setCursorPos(13,i) 
+      term.blit("                       ","fffffffffffffffffffffff","88888888888888888888888")
+    end
+    term.setCursorPos(13,7)
+    term.blit("Config written.","444444444444444","888888888888888")
+    term.setCursorPos(13,8)
+    term.blit("Press any key to exit.","4444444444444444444444","8888888888888888888888")
+    term.setCursorPos(13,10)
+    term.blit("Installed program is","44444444444444444444","88888888888888888888")
+    term.setCursorPos(13,11)
+    term.blit("called 'reactor.lua'.","444444444444444444444","888888888888888888888")
+    os.pullEvent("key")
+    term.setBackgroundColour(colours.black)
+    term.setTextColour(colours.white)
+    term.clear()
+    error("Install successful!")
+  end)
+  -- Cancel installation. This will delete the assets, but keep the libraries (They're useful, mang)
+  button.newButton(29,12,3,2,function()
+    for i=7,13 do
+      term.setCursorPos(13,i) 
+      term.blit("                       ","fffffffffffffffffffffff","88888888888888888888888")
+    end
+    term.setCursorPos(13,7)
+    term.blit("Install cancelled.","eeeeeeeeeeeeeeeeee","888888888888888888")
+    fs.delete("assets/monitor.skimg")
+    fs.delete("assets/terminal.skimg")
+    term.setBackgroundColour(colours.black)
+    term.setTextColour(colours.white)
+    term.clear()
+    error("Exited")
+  end)
+end
+
+main()
---
+++ main.lua
@@ -0,0 +1,263 @@
+-- Prepare variables
+local sUtils = require("sUtils")
+local button = require("button")
+local pg = require("progressBar")
+local monSkimg = sUtils.asset.load("assets/monitor.skimg")
+local termSkimg = sUtils.asset.load("assets/terminal.skimg")
+local config = sUtils.encfread(".reactor.conf")
+local mon = peripheral.wrap(config.monitor)
+local reactor = peripheral.wrap(config.reactor)
+reactor.setAllControlRodLevels(0)
+
+local function difference(a,b)
+  local c = b - a
+  return math.floor((c / a) * 100)
+end
+
+local function gatherData()
+  while true do
+    local rfMax = reactor.getMaxEnergyStored()
+    local rf = reactor.getEnergyStoredUnscaled()
+    local rfLastTick = reactor.getEnergyProducedLastTick()
+    local rfDifference = difference(rfLastTick,rf)
+    local bufferPercentage = math.floor((rf / rfMax) * 100)
+    local fuelTemp = reactor.getFuelTemperature()
+    local caseTemp = reactor.getCaseTemperature()
+    local fuelTempPercent = math.floor((fuelTemp / 2000) * 100)
+    local caseTempPercent = math.floor((caseTemp / 2000) * 100)
+    local active = reactor.getActive()
+    local fuel = reactor.getFuelAmount()
+    local fuelMax = reactor.getFuelAmountMax()
+    local fuelPercent = math.floor((fuel / fuelMax) * 100)
+    local waste = reactor.getWasteAmount()
+    local wastePercent = math.floor((waste / fuelMax) * 100)
+    local activelyCooled = reactor.isActivelyCooled()
+    local controlRodDepth = reactor.getControlRodLevel(0)
+    local water,waterMax,waterPercent,steam,steamMax,steamPercent,steamLastTick,steamMaxLastTick,steamDifference
+    if activelyCooled then
+      water = reactor.getCoolantAmount()
+      waterMax = reactor.getCoolantAmountMax()
+      waterPercent = math.floor((water / waterMax) * 100)
+      steam = reactor.getHotFluidAmount()
+      steamMax = reactor.getHotFluidMax()
+      steamPercent = math.floor((steam / steamMax) * 100)
+      steamDifference = difference(steamLastTick,steam)
+      steamLastTick = reactor.getHotFluidProducedLastTick()
+      steamMaxLastTick = reactor.getMaxHotFluidProducedLastTick()
+    end
+    local data = {
+      rfMax = rfMax, rf = rf, rfLastTick = rfLastTick, rfDifference = rfDifference, bufferPercentage = bufferPercentage, controlRodDepth = controlRodDepth,
+      fuelTemp = fuelTemp, caseTemp = caseTemp, fuelTempPercent = fuelTempPercent, caseTempPercent = caseTempPercent,
+      active = active, fuel = fuel, fuelMax = fuelMax, fuelPercent = fuelPercent, waste = waste, wastePercent = wastePercent,
+      activelyCooled = activelyCooled, water = water, waterMax = waterMax, waterPercent = waterPercent, steam = steam, steamMax = steamMax, steamPercent = steamPercent, steamLastTick = steamLastTick, steamMaxLastTick = steamMaxLastTick, steamDifference = steamDifference,
+    }
+    os.queueEvent("reactorData",data) 
+    sleep(0.05)
+  end
+end
+
+-- Setup progress bars
+-- Colour lookup:
+-- colours.orange: control rod bar 
+-- colour.magenta: fuel bar
+-- colours.lightBlue: waste bar 
+-- colours.yellow: case heat bar 
+-- colours.lime: fuel heat bar 
+-- colours.pink: energy/cold coolant bar 
+-- colours.cyan: hot coolant bar 
+
+local function convertColour(col,barPercentage)
+  local p = barPercentage/100
+  mon.setPaletteColour(col, 1-p,p,0)
+end
+
+local function toSI(x) 
+  local sizes = {"","Ki","Mi","Gi","Ti"}
+  local i = math.floor(math.log(x) / math.log(1024))
+  return tostring(string.format("%0.2f", (x / 1024 ^ i))) .. sizes[i]
+end
+
+local controlBar = pg.create(2,9,37,1,colours.orange,colours.grey,nil,mon)
+local fuelBar = pg.create(2,27,23,1,colours.magenta,nil,mon)
+local wasteBar = pg.create(2,29,23,1,colours.lightBlue,nil,mon)
+local caseHeatBar = pg.create(2,31,23,1,colours.yellow,nil,mon) 
+local fuelHeatBar = pg.create(2,33,23,1,colours.lime,nil,mon) 
+local energyBar = pg.create(2,35,23,1,colours.pink,nil,mon)
+local hotCoolantBar
+if reactor.isActivelyCooled() then
+  hotCoolantBar = pg.create(2,37,23,1,colours.cyan,nil,mon)
+end
+
+local bReactOn = button.newButton(53,34,3,3,function() reactor.setActive(true) end)
+local bReactOff = button.newButton(48,34,3,3,function() reactor.setActive(false) end)
+local bRodIncrease = button.newButton(37,11,3,3,function()
+  local controlRodDepth = reactor.getControlRodLevel(0)
+  if controlRodDepth < 100 then
+    controlRodDepth = controlRodDepth + 1
+    reactor.setAllControlRodLevels(controlRodDepth)
+  end
+end)
+local bRodDecrease = button.newButton(2,11,3,3,function()
+  local controlRodDepth = reactor.getControlRodLevel(0)
+  if controlRodDepth > 0 then
+    controlRodDepth = controlRodDepth - 1
+    reactor.setAllControlRodLevels(controlRodDepth)
+  end
+end)
+
+local function drawData()
+  while true do
+    local _,data = os.pullEvent("reactorData")
+    -- Draw the terminal stuffs first, because this is easy.
+    term.setCursorPos(5,4)
+    term.setBackgroundColour(colours.lightGrey)
+    if data.active then
+      term.setTextColour(colours.lime)
+      term.write("Running")
+    else
+      term.setTextColour(colours.red)
+      term.write("Stopped")
+    end
+    -- Now draw all the other stuffs!
+    -- Reactor toggle button
+    if data.active then
+      for x=30,3 do
+        mon.setCursorPos(52,x) 
+        mon.blit("   ","fff","ddd")
+      end
+    else
+      for x=30,3 do
+        mon.setCursorPos(52,x)
+        mon.blit("   ","fff","eee")
+      end
+    end
+    -- Draw energy outputs, with data
+    if data.activelyCooled then
+      mon.setCursorPos(2,14)
+      mon.blit(" Fluid","000000","bbbbbb")
+      mon.setCursorPos(20,15)
+      mon.blit("B/t   ","000000","bbbbbb")
+      mon.setCursorPos(20,16)
+      mon.blit("B/t   ","000000","bbbbbb")
+      -- Draw the actual data
+      mon.setCursorPos(14,15)
+      mon.setTextColour(colours.white)
+      mon.setBackgroundColour(colours.blue)
+      mon.write(sUtils.cut(tostring(data.steam),5))
+      mon.setCursorPos(14,16)
+      mon.write(sUtils.cut(tostring(data.steamLastTick),5))
+      -- Draw the difference
+      local difString = "-" .. sUtils.cut(tostring(data.steamDifferenceDifference),3) .. "+"
+      mon.setCursorPos(14,17)
+      if data.steamDifferenceDifference < 0 then
+        mon.blit(difString,"e0008","bbbbb")
+      elseif data.steamDifferenceDifference > 0 then
+        mon.blit(difString,"8000d","bbbbb")
+      else
+        mon.blit(difString,"80008","bbbbb")
+      end
+      -- Draw the progress bars
+      mon.setCursorPos(2,34)
+      mon.write("Cold")
+      energyBar:update(data.waterPercent)
+      convertColour(data.waterPercent)
+      mon.setCursorPos(15,34)
+      mon.write(sUtils.cut(data.water .. "/" .. data.waterMax .. "B",11))
+      mon.setCursorPos(2,36)
+      mon.write("Hot")
+      hotCoolantBar:update(data.steamPercent)
+      convertColour(colours.pink,data.steamPercent)
+      mon.setCursorPos(15,36)
+      mon.write(sUtils.cut(data.steam .. "/" .. data.steamMax .. "B",11))
+    else
+      mon.setCursorPos(2,14)
+      mon.blit("Energy","000000","bbbbbb")
+      mon.setCursorPos(20,15)
+      mon.blit("KiRF/t","000000","bbbbbb")
+      mon.setCursorPos(20,16)
+      mon.blit("KiRF/t","000000","bbbbbb")
+      -- Draw the actual data
+      mon.setCursorPos(14,15)
+      mon.setTextColour(colours.white)
+      mon.setBackgroundColour(colours.blue)
+      mon.write(sUtils.cut(tostring(data.rf),5))
+      mon.setCursorPos(14,16)
+      mon.write(sUtils.cut(tostring(data.rfLastTick),5))
+      -- Draw the difference
+      local difString = "-" .. sUtils.cut(tostring(data.rfDifference),3) .. "+"
+      mon.setCursorPos(14,17)
+      if data.rfDifference < 0 then
+        mon.blit(difString,"e0008","bbbbb")
+      elseif data.rfDifference > 0 then
+        mon.blit(difString,"8000d","bbbbb")
+      else
+        mon.blit(difString,"80008","bbbbb")
+      end
+      -- Draw the progress bars
+      mon.setCursorPos(2,34)
+      mon.write("Buffer")
+      energyBar:update(data.bufferPercentage)
+      convertColour(colours.pink,data.bufferPercentage)
+      -- Draw the numbers -- Gonna abuse the free space at the bottom to have more room for this shit
+      local one = toSI(data.rf)
+      local two = toSI(data.rfMax)
+      local str = sUtils.cut(one .. "/" .. two,23)
+      mon.setCursorPos(2,36)
+      mon.write(str)
+    end
+    -- Now draw all the progress bars 
+    -- Setup progress bars
+    -- Colour lookup:
+    -- colours.orange: control rod bar 
+    -- colour.magenta: fuel bar
+    -- colours.lightBlue: waste bar 
+    -- colours.yellow: case heat bar 
+    -- colours.lime: fuel heat bar 
+    -- colours.pink: energy/cold coolant bar 
+    -- colours.cyan: hot coolant bar 
+    -- Fuel bar! 
+    fuelBar:update(data.fuelPercent)
+    convertColour(colours.magenta,data.fuelPercent)
+    wasteBar:update(data.wastePercent)
+    convertColour(colours.lightBlue,data.wastePercent)
+    caseHeatBar:update(data.caseTempPercent)
+    convertColour(colours.yellow,data.caseTempPercent)
+    fuelHeatBar:update(data.fuelTempPercent)
+    convertColour(colours.lime,data.fuelTempPercent)
+    controlBar:update(data.controlRodDepth)
+    convertColour(colours.orange,data.controlRodDepth)
+    -- Draw the control rod percentage 
+    local str = sUtils.cut(data.controlRodDepth .. "%",4)
+    mon.setCursorPos(18,8)
+    mon.write(str)
+  end
+end
+
+local function eventMan()
+  while true do
+    local event = {os.pullEvent()}
+    if event[1] == "monitor_touch" and event[2] == peripheral.getName(mon) then
+      -- Event manipulation, to feed into `button.executeButtons`.
+      local newEvent = {"mouse_click",1,event[3],event[4]}
+      button.executeButtons(newEvent)
+    elseif event[1] == "key" then
+      -- Key got pressed in terminal, lets handle that
+      -- A = Activate reactor, D = Disable reactor, R = Restart pc, S = Shutdown pc 
+      if event[2] == keys.a then
+        reactor.setActive(true) 
+      elseif event[2] == keys.d then
+        reactor.setActive(false) 
+      elseif event[2] == keys.r then 
+        os.reboot() 
+      elseif event[2] == keys.s then 
+        reactor.setActive(false) 
+        os.shutdown() 
+      end
+    end
+  end
+end
+
+sUtils.asset.drawSkimg(monSkimg,nil,nil,mon)
+sUtils.asset.drawSkimg(termSkimg)
+
+parallel.waitForAny(gatherData,drawData,eventMan)
